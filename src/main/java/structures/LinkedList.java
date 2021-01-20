6f58fd30e065311250c62bbbe9397ba6421f02e9
Alan Bustamante <abustamantem06@gmail.com>
2021-01-19 20:30:18 -0700

    create LinkedList class

---
+++ src/main/java/structures/LinkedList.java
@@ -0,0 +1,17 @@
+package structures;
+
+/**
+ * methods:
+ * add
+ * - at head
+ * - at position
+ * - at the end (default)
+ * remove
+ * - position
+ *
+ * other methods?
+ * @param <T>
+ */
+public class LinkedList<T> {
+
+}
